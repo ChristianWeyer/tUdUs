b4508ad35d24668d581c79de4563daf80078f4c0
Christian Weyer <christian.weyer@thinktecture.com>
2012-09-26 15:18:38 +0200

    Fixed the URLs

--- TodoWebApp/app/Scripts/constants.js
+++ TodoWebApp/app/Scripts/constants.js
@@ -1,11 +1,11 @@
-﻿///*
+﻿/*
 serviceEndpointUrl = '../api/todos/';
 pingEndpointUrl = '../api/ping/';
 signalREndpoint = "../signalr";
-//*/
+*/
 
-/*
+///*
 serviceEndpointUrl = 'http://tttodos.azurewebsites.net/api/todos/';
 pingEndpointUrl = 'http://tttodos.azurewebsites.net/api/ping/';
 signalREndpoint = "http://tttodos.azurewebsites.net/signalr";
-*/
+//*/
