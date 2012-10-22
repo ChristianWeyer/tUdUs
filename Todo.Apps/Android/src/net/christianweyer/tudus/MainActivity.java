3e2651d9acb8feeb6d6bacd5bbb32d962d4e4918
Christian Weyer <christian.weyer@thinktecture.com>
2012-10-22 13:30:13 +0200

    Added an Android PhoneGap project (needs sym link)

---
+++ Todo.Apps/Android/src/net/christianweyer/tudus/MainActivity.java
@@ -0,0 +1,15 @@
+package net.christianweyer.tudus;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/views/index.html");
+    }
+}
