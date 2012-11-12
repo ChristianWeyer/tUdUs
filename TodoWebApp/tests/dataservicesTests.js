4e64f12c132036c464b2e0e0bba5703cba8ea24a
Christian Weyer <christian.weyer@thinktecture.com>
2012-11-12 21:44:00 +0100

    started clean-up for testing rigs

--- TodoWebApp/tests/dataservicesTests.js
+++ TodoWebApp/tests/dataservicesTests.js
@@ -1,37 +1,24 @@
-﻿/// <reference path="../app/js/jquery-1.7.1.min.js" />
-/// <reference path="../app/js/jquery.base64.min.js" />
-/// <reference path="../app/js/underscore-min.js" />
-/// <reference path="../app/js/kendo.mobile.min.js" />
-/// <reference path="../app/js/amplify.min.js" />
-/// <reference path="../app/js/jquery.signalR-0.5.3.min.js" />
-/// <reference path="../app/js/notifier.min.js" />
-/// <reference path="../app/js/signalr.hubs.js" />
-/// <reference path="../app/js/jquery.ba-bbq.min.js" />
-
-/// <reference path="../app/scripts/app.js" />
-/// <reference path="../app/scripts/applicationConstants.js" />
-/// <reference path="../app/scripts/views.js" />
-/// <reference path="../app/scripts/services/httpConstants.js" />
-/// <reference path="../app/scripts/services/notifications.js" />
-/// <reference path="../app/scripts/services/dataservices.js" />
-/// <reference path="../app/scripts/viewmodels/authenticationViewModel.js" />
-/// <reference path="../app/scripts/viewmodels/newTodoItemViewModel.js" />
-/// <reference path="../app/scripts/viewmodels/todosViewModel.js" />
-/// <reference path="../app/scripts/viewmodels/errorViewModel.js" />
-/// <reference path="../app/scripts/viewmodels/acsViewModel.js" />
-/// <reference path="../app/scripts/viewmodels/settingsViewModel.js" />
+﻿/// <reference path="../www/js/libs/jquery-1.7.1.min.js" />
+/// <reference path="../www/js/libs/jquery.base64.min.js" />
+/// <reference path="../www/js/libs/amplify.min.js" />
+/// <reference path="../www/js/applicationConstants.js" />
+/// <reference path="../www/js/services/httpConstants.js" />
+/// <reference path="../www/js/services/dataservices.js" />
 
 // NOT functional at the moment
-module("dataservices", {
+module("dataservicesTests", {
     setup: function () {
-        window.kendoMobileApplication = new kendo.mobile.Application($(document.body), {
-            transition: 'slide'
-        });
+        //window.kendoMobileApplication = new kendo.mobile.Application($(document.body), {
+        //    transition: 'none'
+        //});
     },
     teardown: function () {        
     }
 });
 
-test("callLoginPing_WithCorrectUsernameAndPassword", function () {
-    var login = dataservices.callLoginPing("w", "w");
+test("callLoginPing_WithCorrectUsernameAndPassword", function() {
+    dataservices.loginPing("w", "w")
+        .done(function () {
+            alert("Success");
+        });
 });
