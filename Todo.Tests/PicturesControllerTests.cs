059bcb36ab54464c0fcd0e51d3a3be31dc7d0bbb
Christian Weyer <christian.weyer@thinktecture.com>
2012-12-03 21:32:15 +0100

    Fixed EF NuGet issues and added GETting images

---
+++ Todo.Tests/PicturesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.DataAccess;
+using Todo.WebApi;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Todo.Tests
+{
+    [TestClass]
+    public class PicturesControllerTests
+    {
+        [TestMethod]
+        public void Test_Get_Pictures_For_Given_User()
+        {
+            Thread.CurrentPrincipal =
+                new GenericPrincipal(new GenericIdentity("cw"), null);
+            
+            var pc = new PicturesController(
+                new TodoRepository());
+            var pics = pc.Get().ToList();
+
+            Assert.IsNotNull(pics);
+            Assert.IsTrue(pics.Count > 0);
+        }
+    }
+}
--- Todo.WebApi/PicturesController.cs
+++ Todo.WebApi/PicturesController.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Todo.Base;
+using Todo.Entities;
 
 namespace Todo.WebApi
 {
     /// <summary>
     /// Controller for uploading pictures
     /// </summary>
-    [AllowAnonymous]
     public class PicturesController : ApiController
     {
+        private readonly IGenericRepository<TodoItem> repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PicturesController" /> class.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        public PicturesController(IGenericRepository<TodoItem> repository)
+        {
+            this.repository = repository;
+        }
         /// <summary>
         /// Post pictures to this action.
         /// </summary>
         /// <returns></returns>
         /// <exception cref="System.Web.Http.HttpResponseException"></exception>
+        [AllowAnonymous]
         public Task<List<FileDetails>> Post()
         {
             if (!Request.Content.IsMimeMultipartContent("form-data"))
@@ -25,7 +38,7 @@ namespace Todo.WebApi
             }
 
             var multipartStreamProvider = new BlobStorageMultipartProvider(BlobHelper.GetContainer());
-            
+
             return Request.Content.ReadAsMultipartAsync<BlobStorageMultipartProvider>(multipartStreamProvider).ContinueWith<List<FileDetails>>(t =>
             {
                 if (t.IsFaulted)
@@ -41,6 +54,14 @@ namespace Todo.WebApi
 
                 return provider.Files;
             });
+        }
+
+        [Queryable]
+        public IQueryable<string> Get()
+        {
+            var todos = repository.FindBy(t => t.Owner == User.Identity.Name);
+
+            return todos.Select(t => t.PictureUrl);
         }
     }
 
