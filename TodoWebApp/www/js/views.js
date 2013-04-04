2c02991ae09ec1399873fdd0bb074e69aa7b1162
Christian Weyer <christian.weyer@thinktecture.com>
2013-04-04 10:59:42 +0200

    Perf testing things

--- TodoWebApp/www/js/views.js
+++ TodoWebApp/www/js/views.js
@@ -20,14 +20,14 @@ todosApp.Views.todoDetailsShow = function (e) {
     todosViewModel.set("currentItem", todosViewModel.todosSource.get(e.view.params.id));
     e.view.scroller.reset();
     
-    var loc = todosViewModel.get("currentItem.location");
-
+    //var loc = todosViewModel.get("currentItem.location");
+	/*
     if(loc != null) {
         var coords = loc.split(",");
         todosApp.Views.createMap(coords[0], coords[1], "detailsMap");
     } else {
         
-    }
+    }*/
 };
 
 todosApp.Views.tapped = function (e) {
