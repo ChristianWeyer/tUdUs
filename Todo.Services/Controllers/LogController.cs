dfbc1fef0b0df8d90f8b6137cff94db979f17dab
Christian Weyer <christian.weyer@thinktecture.com>
2013-04-08 14:31:32 +0200

    Playing with dynamic logging (server)

--- Todo.Services/Controllers/LogController.cs
+++ Todo.Services/Controllers/LogController.cs
@@ -1,5 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Web.Http;
+using MongoDB.Driver;
+using Simple.Data;
+using Simple.Data.MongoDB;
 
 namespace Todo.Services
 {
@@ -13,20 +19,59 @@ namespace Todo.Services
         /// Post specified log data.
         /// </summary>
         /// <param name="logData">The log data.</param>
-        public void Post(dynamic logData)
+        public void Post(LogData logData)
         {
-            // TODO: persist logging messages - consider using a JSON DB (MongoDB?)
-            /* E.g. JSON structure:
-                'logger',
-                'timestamp',
-                'level',
-                'url',
-                'message',
-                'exception'
-             */
-
-            string msg = logData.message;
-            Debug.WriteLine("###Log message from client: {0}", msg);
+            // TODO: persist logging messages - consider EF code-first
+            Debug.WriteLine("###Log message from client: {0}", logData.Message);
         }
+
+        ///// <summary>
+        ///// Post specified log data (dynamic version).
+        ///// </summary>
+        ///// <param name="logData">The log data.</param>
+        //public void Post(dynamic logData)
+        //{
+        //    // TODO: persist logging messages - consider using a JSON DB (MongoDB?)
+        //    /* E.g. JSON structure:
+        //        'logger',
+        //        'timestamp',
+        //        'level',
+        //        'url',
+        //        'message',
+        //        'exception'
+        //     */
+
+        //    try
+        //    {
+        //        var db = DatabaseHelper.Open();
+        //        db.Logs.Insert(logData); // does not work; either further investigate - or wait for dynamic support in official MongoDB driver 2.0
+        //    }
+        //    catch (Exception)
+        //    {
+        //        // NOTE: swallow intentionally for the demo app
+        //        //throw;
+        //    }
+
+        //    string msg = logData.message;
+        //    Debug.WriteLine("###Log message from client: {0}", msg);
+        //}
+
+        //internal static class DatabaseHelper
+        //{
+        //    public static dynamic Open()
+        //    {
+        //        return Database.Opener.OpenMongo("mongodb://localhost/todosLogging?safe=true");
+        //    }
+        //}
+    }
+
+    public class LogData
+    {
+        public string Logger { get; set; }
+        public string Timestamp { get; set; }
+        public string Level { get; set; }
+        public string Url { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
     }
 }
