96458bcedd8d1b2406541e1468858d6b38348a32
Christian Weyer <christian.weyer@thinktecture.com>
2013-05-06 12:31:00 +0200

    Added simple jQM client

---
+++ TodoWebApp/jqm/controllers.js
@@ -0,0 +1,28 @@
+function TodoController($scope, Todo) {
+    $scope.todos = Todo.query();
+
+    $scope.selectTodo = function(id) {
+        $scope.todo = _.where($scope.todos, { id: id })[0];
+    };
+
+    $scope.newTodo = function() {
+        $scope.todo = new Todo();
+    };
+
+    $scope.saveTodo = function() {
+        if ($scope.todo.id == null) {
+            Todo.save({}, $scope.todo, function(data) {
+                $scope.todos.push(data);
+            });
+        } else {
+            Todo.update({ todoId: $scope.todo.id }, $scope.todo, function(data) {
+            });
+        }
+    };
+
+    $scope.completeTodo = function(id) {
+        Todo.delete({ todoId: id }, function() {
+            $scope.todos = _.without($scope.todos, $scope.todo);
+        });
+    };
+}
