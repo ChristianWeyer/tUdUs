981d8aa23c317c41ca848b28ec5c24882bc694a1
Christian Weyer <christian.weyer@thinktecture.com>
2013-05-18 09:28:17 +0200

    Changed base URL

--- TodoWebApp/www/js/applicationConstants.js
+++ TodoWebApp/www/js/applicationConstants.js
@@ -1,7 +1,7 @@
 getBaseUrl = function () {
     var useSelfHost = false;
-    //var host = "http://tttodos.azurewebsites.net/";
-    var host = "http://localhost:7778/";
+    var host = "http://tttodos.azurewebsites.net/";
+    //var host = "http://localhost:7778/";
     var baseUrl = host;
     
     if (!(ttTools.isInApp() || ttTools.isInPhoneGapApp() || useSelfHost)) {
